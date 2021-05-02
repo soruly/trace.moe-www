079cdab43e7fd0ddaea263b7d70a4025417b9f67
soruly <soruly@gmail.com>
2021-05-03 03:19:51 +0800

    Add account login page

--- static/js/account.js
+++ static/js/account.js
@@ -1,13 +1,123 @@
+const renderPage = async (e) => {
+  document.querySelector(".account-id").innerText = "...";
+  document.querySelector(".account-type").innerText = "...";
+  document.querySelector(".account-email").innerText = "...";
+  document.querySelector(".account-quota").innerText = "...";
+  document.querySelector(".account-rate-limit").innerText = "...";
+  document.querySelector(".account-concurrency-limit").innerText = "...";
+  document.querySelector(".meter-fg").setAttribute("width", "0%");
+  let apiKey = localStorage.getItem("apiKey");
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  let res = await fetch(`https://api.trace.moe/me${apiKey ? `?key=${apiKey}` : ""}`);
+  if (res.status >= 400) {
+    localStorage.removeItem("apiKey");
+    apiKey = null;
+    res = await fetch("https://api.trace.moe/me");
+  }
+  const user = await res.json();
+
+  if (user) {
+    document.querySelector(".account-id").innerText = user.id;
+    document.querySelector(".account-type").innerText = user.email ? "User" : "Guest";
+    document.querySelector(".account-email").innerText = user.email || "N/A";
+    document.querySelector(".account-quota").innerText = `${user.quotaUsed} / ${user.quota}`;
+    document.querySelector(".account-rate-limit").innerText = `${user.rateLimit} per minute`;
+    document.querySelector(".account-concurrency-limit").innerText = `${user.concurrency}`;
+    document
+      .querySelector(".meter-fg")
+      .setAttribute("width", `${(user.quotaUsed / user.quota) * 100}%`);
+  }
+
+  if (apiKey) {
+    document.querySelector(".box.login").classList.add("hidden");
+    document.querySelector(".box.developer").classList.remove("hidden");
+    document.querySelector(".box.security").classList.remove("hidden");
+    document.querySelector(".box.logout").classList.remove("hidden");
+    document.querySelector("#api-key").value = apiKey;
+  } else {
+    document.querySelector(".box.login").classList.remove("hidden");
+    document.querySelector(".box.developer").classList.add("hidden");
+    document.querySelector(".box.security").classList.add("hidden");
+    document.querySelector(".box.logout").classList.add("hidden");
+    document.querySelector("#api-key").value = "";
+  }
+  document.querySelector("#password-label").innerText = "";
+  document.querySelector("#api-key-label").innerText = "";
+};
+
 (async () => {
-  const user = await fetch("https://api.trace.moe/me").then((e) => e.json());
-  document.querySelector("#account-id").innerText = user.id;
-  document.querySelector("#account-type").innerText = user.email ? "User" : "Guest";
-  document.querySelector("#account-email").innerText = user.email || "N/A";
-  document.querySelector("#account-quota").innerText = `${user.quotaUsed} / ${user.quota}`;
-  document.querySelector("#account-rate-limit").innerText = `${user.rateLimit} per minute`;
-  document.querySelector("#account-concurrency-limit").innerText = `${user.concurrency}`;
-  document
-    .querySelector(".meter-fg")
-    .setAttribute("width", `${(user.quotaUsed / user.quota) * 100}%`);
-  document.querySelector(".account-info").style.opacity = 1;
+  await renderPage();
 })();
+
+document.querySelector(".login form").onsubmit = async (e) => {
+  e.preventDefault();
+  document.querySelectorAll(".login form input").forEach((e) => {
+    e.disabled = true;
+  });
+  document.querySelector("#login-label").classList.remove("error");
+  document.querySelector("#login-label").innerText = "";
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
+  document.querySelector("#login-label").innerText = "Logging in...";
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  if (Math.random() > 0.5) {
+    localStorage.setItem("apiKey", "xxxxxxxxxxxxxxxxxxxxxxxxx");
+    await renderPage();
+    document.querySelector("#login-label").innerText = "";
+  } else {
+    document.querySelector("#login-label").innerText = "Invalid Email / Wrong Password";
+    document.querySelector("#login-label").classList.add("error");
+  }
+
+  document.querySelectorAll(".login form input").forEach((e) => {
+    e.disabled = false;
+  });
+};
+
+document.querySelector(".logout-btn").onclick = async (e) => {
+  document.querySelector(".logout-btn").disabled = true;
+  localStorage.removeItem("apiKey");
+  await renderPage();
+  document.querySelector(".logout-btn").disabled = false;
+};
+
+document.querySelector(".security form").onsubmit = async (e) => {
+  e.preventDefault();
+  document.querySelectorAll(".security form input").forEach((e) => {
+    e.disabled = true;
+  });
+  document.querySelector("#password-label").classList.remove("error");
+  document.querySelector("#password-label").innerText = "";
+  const password = document.querySelector("#new-password").value;
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  if (Math.random() > 0.5) {
+    document.querySelector("#password-label").innerText = "Password has changed successfully.";
+  } else {
+    document.querySelector("#password-label").innerText = "Something went wrong";
+    document.querySelector("#password-label").classList.add("error");
+  }
+  document.querySelector("#new-password").value = "";
+  document.querySelectorAll(".security form input").forEach((e) => {
+    e.disabled = false;
+  });
+};
+
+document.querySelector(".developer form").onsubmit = async (e) => {
+  e.preventDefault();
+  document.querySelectorAll(".developer form input").forEach((e) => {
+    e.disabled = true;
+  });
+  document.querySelector("#api-key-label").classList.remove("error");
+  document.querySelector("#api-key-label").innerText = "";
+
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  if (Math.random() > 0.5) {
+    document.querySelector("#api-key-label").innerText = "API Key has been reset.";
+  } else {
+    document.querySelector("#api-key-label").innerText = "Something went wrong";
+    document.querySelector("#api-key-label").classList.add("error");
+  }
+  document.querySelectorAll(".developer form input").forEach((e) => {
+    e.disabled = false;
+  });
+};

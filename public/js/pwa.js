df74c62124c4bbbdbc16cd359a4dbaaf98181d14
soruly <soruly@gmail.com>
2022-05-28 05:39:36 +0000

    Add image file handler to PWA

--- public/js/pwa.js
+++ public/js/pwa.js
@@ -1,5 +1,18 @@
 (async () => {
+  if (!("launchQueue" in window)) return;
+  if (!("files" in LaunchParams.prototype)) return;
+
+  window.launchQueue.setConsumer(async (launchParams) => {
+    if (!launchParams.files.length) return;
+    const canvas = document.createElement("canvas");
+    const fileContents = await launchParams.files[0].getFile();
+    const imageBitmap = await createImageBitmap(fileContents);
+    canvas.width = imageBitmap.width;
+    canvas.height = imageBitmap.height;
+    canvas.getContext("2d").drawImage(imageBitmap, 0, 0, imageBitmap.width, imageBitmap.height);
+    document.querySelector("#originalImage").src = canvas.toDataURL("image/jpeg", 0.9);
+  });
+
   if (!navigator.serviceWorker) return;
-  if (navigator.userAgent.includes("Mac") && "ontouchend" in document) return; // iOS
   navigator.serviceWorker.register("/sw.js");
 })();

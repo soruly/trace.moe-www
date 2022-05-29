49b2c8f2a8c704284bfb5f6b936539537b85aff1
soruly <soruly@gmail.com>
2022-05-29 05:41:38 +0000

    Improve error handling on service worker

--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,32 @@
 self.addEventListener("fetch", (event) => {
   const url = new URL(event.request.url);
   // incoming POST request for PWA share_target
-  if (event.request.method === "POST" && url.pathname === "/") {
+  if (event.request.method === "POST" && url.origin === self.origin && url.pathname === "/") {
     event.respondWith(
       (async () => {
-        const request = new Request("/");
-        const res = await fetch(request);
-        const formData = await event.request.formData();
-        // if (!formData) return res;
-        const file = formData.get("image");
+        const req = event.request.clone();
+        try {
+          const formData = await event.request.formData();
+          const file = formData.get("image");
 
-        const headers = new Headers();
-        headers.set("Content-Type", file.type);
-        const response = new Response(file.stream(), { headers });
-        const cache = await caches.open("cache");
-        cache.put(new Request("/temp.image"), response);
+          const headers = new Headers();
+          headers.set("Content-Type", file.type);
+          const response = new Response(file.stream(), { headers });
+          const cache = await caches.open("cache");
+          cache.put(new Request("/temp.image"), response);
 
-        return Response.redirect(`${event.request.url}?url=${event.request.url}temp.image`, 302);
+          return Response.redirect(`${event.request.url}?url=${event.request.url}temp.image`, 302);
+        } catch (e) {
+          return fetch(req);
+        }
       })()
     );
   }
-  if (event.request.method === "GET" && url.pathname === "/temp.image") {
+  if (
+    event.request.method === "GET" &&
+    url.origin === self.origin &&
+    url.pathname === "/temp.image"
+  ) {
     event.respondWith(caches.match(event.request));
   }
 

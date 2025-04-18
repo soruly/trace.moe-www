3d3880a4a1db4a56b00013a7c363a627de6d521c
ソ瑠璃(soruly) <soruly@gmail.com>
2025-04-18 10:12:28 +0800

    Fix decimal in time format

--- components/utils.js
+++ components/utils.js
@@ -1,5 +1,5 @@
 const formatTime = (timeInSeconds) => {
-  const sec_num = parseInt(timeInSeconds, 10);
+  const sec_num = Math.round(parseInt(timeInSeconds, 10));
   const hours = Math.floor(sec_num / 3600);
   const minutes = Math.floor((sec_num - hours * 3600) / 60);
   const seconds = sec_num - hours * 3600 - minutes * 60;

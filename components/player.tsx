8df311723dadb344d5619ad024d6442d9c5a27d8
soruly <soruly@gmail.com>
2026-02-14 16:13:26 +0900

    Refactor player style

--- components/player.tsx
+++ components/player.tsx
@@ -11,100 +11,72 @@ export default function Player({
   isLoading,
   isSearching,
 }) {
+  const videoRef = useRef(null);
   const playerRef = useRef(null);
+  const progressBarRef = useRef(null);
   const [isMute, setIsMute] = useState(true);
-  const [playerWidth, setPlayerWidth] = useState(window.innerWidth > 640 ? 640 : window.innerWidth);
-  const [playerHeight, setPlayerHeight] = useState(360);
-  const [videoWidth, setVideoWidth] = useState(640);
-  const [videoHeight, setVideoHeight] = useState(360);
+  const [playerVisible, setPlayerVisible] = useState(false);
   const [playerSrc, setPlayerSrc] = useState(null);
   const [playerLoading, setPlayerLoading] = useState(isLoading);
   const [playerLoadingError, setPlayerLoadingError] = useState(false);
   const [dropTargetText, setDropTargetText] = useState("");
 
   const playPause = function () {
-    if (playerRef.current.paused) {
-      playerRef.current.play();
+    if (videoRef.current.paused) {
+      videoRef.current.play();
     } else {
-      playerRef.current.pause();
+      videoRef.current.pause();
     }
   };
-
-  useEffect(() => {
-    let animationFrameId;
-    const handleResize = () => {
-      cancelAnimationFrame(animationFrameId);
-      animationFrameId = requestAnimationFrame(() => {
-        const newWidth = window.innerWidth > 640 ? 640 : window.innerWidth;
-        setPlayerWidth(newWidth);
-        setPlayerHeight((newWidth / videoWidth) * videoHeight);
-      });
-    };
-    window.addEventListener("resize", handleResize, false);
-    return () => {
-      window.removeEventListener("resize", handleResize, false);
-      cancelAnimationFrame(animationFrameId);
-    };
-  }, [videoWidth, videoHeight]);
 
   useEffect(() => {
     if (!src) {
       setPlayerLoadingError(true);
       return;
     }
-    playerRef.current.pause();
+    videoRef.current.pause();
     setPlayerLoading(true);
     setPlayerLoadingError(false);
-    playerRef.current.style.opacity = 0;
+    setPlayerVisible(false);
     setPlayerSrc(`${src}?size=l`);
   }, [src]);
 
   useEffect(() => {
     if (src) return;
     if (isSearching) {
-      playerRef.current.pause();
+      videoRef.current.pause();
       setPlayerLoading(true);
       setPlayerLoadingError(false);
-      playerRef.current.style.opacity = 0;
+      setPlayerVisible(false);
       setPlayerSrc(null);
     } else {
       setPlayerLoading(false);
     }
   }, [isSearching]);
 
+  progressBarRef.current?.style.setProperty("--progress", `${timeCode / duration}`);
+
   return (
-    <div className={styles.playerPane}>
-      {playerLoadingError ? (
-        <div className={styles.playerError} style={{ height: playerHeight, width: playerWidth }}>
-          No video available.
-        </div>
-      ) : null}
+    <div ref={playerRef} className={styles.playerPane}>
+      {playerLoadingError ? <div className={styles.playerError}>No video available.</div> : null}
       <video
-        ref={playerRef}
-        className={styles.player}
+        ref={videoRef}
+        className={`${styles.player} ${playerVisible ? styles.visible : ""}`}
         src={playerSrc}
-        width={playerWidth}
-        height={playerHeight}
         muted={isMute}
         autoPlay
         loop
         playsInline
         onLoadedMetadata={(e: React.SyntheticEvent<HTMLVideoElement>) => {
-          e.currentTarget.style.opacity = "1";
+          setPlayerVisible(true);
           if (e.currentTarget.videoWidth && e.currentTarget.videoHeight) {
-            setVideoWidth(e.currentTarget.videoWidth);
-            setVideoHeight(e.currentTarget.videoHeight);
-            setPlayerHeight(
-              (playerWidth / e.currentTarget.videoWidth) * e.currentTarget.videoHeight,
-            );
-          } else {
-            setPlayerHeight(360);
+            playerRef.current.style.aspectRatio = `${e.currentTarget.videoWidth} / ${e.currentTarget.videoHeight}`;
           }
         }}
         onClick={playPause}
         onCanPlayThrough={(e: React.SyntheticEvent<HTMLVideoElement>) => {
           setPlayerLoading(false);
-          playerRef.current.play();
+          videoRef.current.play();
         }}
         onContextMenu={(e: React.MouseEvent<HTMLVideoElement>) => {
           e.preventDefault();
@@ -112,7 +84,6 @@ export default function Player({
       ></video>
       <div
         className={styles.dropEffect}
-        style={{ height: playerHeight - 10 }}
         onClick={playPause}
         onDrop={(e: React.DragEvent<HTMLDivElement>) => {
           const result = onDrop(e);
@@ -140,14 +111,8 @@ export default function Player({
       >
         {dropTargetText}
       </div>
-      <div
-        className={styles.loading}
-        style={{ height: playerHeight, display: playerLoading || isSearching ? "flex" : "none" }}
-      >
-        <div
-          className={playerLoading || isSearching ? styles.ripple : ""}
-          style={{ height: (playerHeight - 800) / 2 }}
-        ></div>
+      <div className={`${styles.loading} ${playerLoading || isSearching ? styles.active : ""}`}>
+        <div className={playerLoading || isSearching ? styles.ripple : ""}></div>
       </div>
 
       <div className={styles.playerInfo}>
@@ -159,11 +124,8 @@ export default function Player({
       </div>
       <div className={styles.playerControl}>
         <div
-          className={styles.progressBarControl}
-          style={{
-            animationName: isSearching ? styles.seek : "none",
-            left: (timeCode / duration) * playerWidth - 6 || 0,
-          }}
+          ref={progressBarRef}
+          className={`${styles.progressBarControl} ${isSearching ? styles.seeking : ""}`}
         >
           ▲
         </div>
@@ -171,7 +133,7 @@ export default function Player({
           className={`${styles.soundBtn} ${isMute ? styles.iconVolumeOff : styles.iconVolumeUp}`}
           onClick={() => {
             setIsMute(!isMute);
-            playerRef.current.muted = !isMute;
+            videoRef.current.muted = !isMute;
           }}
         ></div>
       </div>
